c0597bff5b3d5a3d0a64b5f18c5e4d72c2d05d2e
Stanford Chen <stanfordchen2007@gmail.com>
2025-01-14 23:03:12 -0500

    Removed Photos and Completed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let users = []
 let posts = []
 let albums = []
-let photos = new Array(5000)
 let userId = 0
 
 async function getUsers() {
@@ -43,13 +42,6 @@ async function getAlbums() {
 }
 getAlbums()
 
-async function getPhotos(albumId) {
-    for (let i = 50 * (albumId - 1); i < 50 * albumId; i++) {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${i + 1}`)
-        photos[i] = await response.json()
-    }
-}
-
 function selectUser(username) {
     for (let i = 0; i < users.length; i++) {
         if (users[i].username == username) {
@@ -60,8 +52,9 @@ function selectUser(username) {
 }
 
 async function loadUser() {
+    document.getElementById("profile").style.visibility = "visible"
     let div = document.getElementById("posts")
-    div.innerHTML = ""
+    div.innerHTML = `<h2>${users[userId-1].username}'s posts`
     for (let i = 0; i < posts.length; i++) {
         if (posts[i].userId == userId) {
             let x = i
@@ -74,18 +67,13 @@ async function loadUser() {
         }
     }
     div = document.getElementById("albums")
-    div.innerHTML = ""
+    div.innerHTML = `<h2>${users[userId-1].username}'s albums`
     for (let i = 0; i < albums.length; i++) {
         if (albums[i].userId == userId) {
             let x = i
             while (albums[x].userId == userId) {
                 let album = albums[x]            
                 div.innerHTML += `<h3>${album.title}</h3>`
-                await getPhotos(album.id)
-                for (let y = 50 * (album.id - 1); y < 50 * album.id; y++) {
-                    let img = photos[y].thumbnailUrl
-                    div.innerHTML += `<img loading="lazy" id="photo" src="${img}"/>`
-                }
                 x++
             }
             break
